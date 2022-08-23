5e6ff053267410911f5681cd2c66083a6c36b448
Anthony Lizano <106571575+joetony@users.noreply.github.com>
2022-08-22 23:41:36 -0600

    Add files via upload
    category section was added and changes were made in the creation of products for the same category reason.

--- project/js/class.js
+++ project/js/class.js
@@ -64,7 +64,7 @@ function checkEmailFormat() {
 function emailValidation() {
   // read users from the database
   const users = JSON.parse(localStorage.getItem('users'));
- 
+
   const email = $('#email').val();
 
 
@@ -176,12 +176,12 @@ function loginValidation() {
 //load name of user on screen
 function loadUserNameLoggedIn() {
   let user = JSON.parse(sessionStorage.getItem('user'));
-  if (user){
+  if (user) {
     document.getElementById('userNameLoggedIn').innerHTML = user.name;
   }
-  }
+}
 
-  
+
 
 //check if an user is logged in
 function checkUserLoggedIn() {
@@ -225,6 +225,13 @@ function validateProductInputs() {
     return false;
   }
 
+  var selectedValue = category.options[category.selectedIndex] ? category.options[category.selectedIndex].value : null;
+
+  if (!selectedValue) {
+    alert("Se debe seleccionar una Categoría.");
+    return false;
+  }
+
   return true;
 
 }
@@ -243,6 +250,8 @@ function addProduct() {
     const productDescription = $('#description').val();
     const productImage = $('#image').val();
     const productSwap = $('#swap').val();
+    const productCategory = $('#category option:selected').text();
+   
 
     let currentProductID = JSON.parse(sessionStorage.getItem('product'));
     let productsDb = JSON.parse(localStorage.getItem('products'));
@@ -258,6 +267,7 @@ function addProduct() {
           productsDb[i].description = productDescription;
           productsDb[i].image = productImage;
           productsDb[i].swap = productSwap;
+          productsDb[i].category = productCategory;
           break;
         }
       }
@@ -277,6 +287,7 @@ function addProduct() {
         description: productDescription,
         image: productImage,
         swap: productSwap,
+        category:productCategory
 
       }
       productsDb.push(product);
@@ -361,6 +372,7 @@ function saveCurrentProduct(product) {
   sessionStorage.setItem('product', JSON.stringify(product));
 }
 function deleteCurrentProduct() {
+  deleteCurrentCategory();
   sessionStorage.removeItem("product");
 }
 //check if product if being edited
@@ -377,6 +389,10 @@ function loadDataOfEditProduct() {
         $('#description').val(currentProduct[i].description);
         $('#image').val(currentProduct[i].image);
         $('#swap').val(currentProduct[i].swap);
+        $('#category').val(currentProduct[i].category);
+        
+        $("#category option:contains(" + currentProduct[i].category + ")").attr('selected', 'selected');
+
 
       }
     }
@@ -401,6 +417,7 @@ function loadProductInformation() {
 
         document.getElementById('name').innerHTML = `<h1>${currentProduct[i].name}</h1> `;
         document.getElementById('userEmail').innerHTML = currentProduct[i].user;
+        document.getElementById('category').innerHTML = currentProduct[i].category;
         document.getElementById('description').innerHTML = `<p>${currentProduct[i].description}</p>`;
         document.getElementById('image').innerHTML = `<img class="img-fluid" src="${currentProduct[i].image}" alt="img">`;
         document.getElementById('swap').innerHTML = `<p>${currentProduct[i].swap}</p>`;
@@ -457,12 +474,12 @@ function loadRecentCambalaches() {
   for (var i = products.length - 1; i >= 0; i--) {
     if (counter != 0) {
       counter--;
-      productsContainer += `<div class="col-12 col-sm-5 col-md-6 col-lg-5 mb-4 mx-lg-auto mx-md-auto mx-sm-auto">
+      productsContainer += `<div class="col-6 col-sm-6 col-xs-6">
       <a  href="http://cambalachefinal.net:8080/detalle.html" onclick="saveCurrentProduct(${products[i].id});">
     <img src="${products[i].image}" class=" w-50" alt="img">
     <p>${products[i].name}</p></a></div>`;
     } else {
-      productsContainerAdditional += `<div class="col-12 col-sm-5 col-md-6 col-lg-5 mb-4 mx-lg-auto mx-md-auto mx-sm-auto">
+      productsContainerAdditional += `<div class="col-6 col-sm-6 col-xs-6">
       <a  href="http://cambalachefinal.net:8080/detalle.html" onclick="saveCurrentProduct(${products[i].id});">
     <img src="${products[i].image}" class=" w-50" alt="img">
     <p>${products[i].name}</p></a></div>`;
@@ -474,5 +491,212 @@ function loadRecentCambalaches() {
   document.getElementById('productsContainerAdditional').innerHTML = productsContainerAdditional;
 
 }
+//categories section             ----------------------------------------------------------------------------------------------------------
+function saveCurrentCategory(category) {
+  sessionStorage.setItem('category', JSON.stringify(category));
+}
+function deleteCurrentCategory() {
+  sessionStorage.removeItem("category");
+}
+
+//delete category
+function deleteCategory(categoryID) {
+  //debugger
+  let categories = JSON.parse(localStorage.getItem('categories'));
+  for (var i = 0; i < categories.length; i++) {
+    if (categories[i].id == categoryID) {
+      categories.splice(i, 1);
+      alert("Se ha eliminado la categoría correctamente.");
+      window.location.reload();
+      break;
+
+    }
+  }
+
+  localStorage.setItem('categories', JSON.stringify(categories));
+}
+//load  product categories
+function loadDashboardCategories() {
+  const categories = JSON.parse(localStorage.getItem('categories'));
+  //let select= document.getElementById("authors-list");
+
+
+  if (categories) {
+    let options = "";
+    for (var i = 0; i < categories.length; i++) {
+
+      options += `<div class="col-lg-4 my-3 col-xs-6 col-sm-6 col-md-6">
+                       
+        <div class="row">
+            <div class="col d-flex justify-content-end">
+        
+                <img class="border-img img-responsive rounded" src="${categories[i].image}" alt="img">
+            </div>
+        
+        
+            <div class="col d-flex align-items-start flex-column">
+                <div class="row">
+                    <div class="col">
+                        <a href="#">${categories[i].name}</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <button class="greenbutton"
+                            onclick="location.href='http://cambalachefinal.net:8080/categorias.html';saveCurrentCategory(${categories[i].id})">Editar</button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col"><button class="redbutton"
+                            onclick="deleteCategory(${categories[i].id})">Eliminar</button>
+                    </div>
+                </div>
+        
+            </div>
+        </div>
+        </div>`;
+
+
+    }
+    // renders the select authors-list with the authors found
+    document.getElementById('rowCategoriesDashboard').innerHTML = options;
+  }
+}
+
+
+//add new category
+function addCategory() {
+  if (validateCategoryInputs()) {
+
+    const categoryName = $('#name').val();
+    const categoryImage = $('#image').val();
+
+    let currentCategoryID = JSON.parse(sessionStorage.getItem('category'));
+    let categoriesDb = JSON.parse(localStorage.getItem('categories'));
+    if (!categoriesDb) {
+      categoriesDb = [];
+    }
+
+    if (currentCategoryID) {//check if a category was selected  
+      for (var i = 0; i < categoriesDb.length; i++) {
+        if (categoriesDb[i].id == currentCategoryID) {
+          categoriesDb[i].name = productName;
+          categoriesDb[i].image = productImage;
+          break;
+        }
+      }
+    } else {
+
+      //insert to a database
+
+
+      const category = {
+        id: categoriesDb.length + 1,
+        name: categoryName,
+        image: categoryImage
+
+      }
+      categoriesDb.push(category);
+
+    }
+    localStorage.setItem('categories', JSON.stringify(categoriesDb));
+
+    alert("Se ha guardado la categoria correctamente.");
+    window.location.href = 'http://cambalachefinal.net:8080/dashboard-categorias.html';
+  }
+}
+//validate category inputs
+function validateCategoryInputs() {
+  if (document.forms["categoryForm"]["name"].value == "") {//change id form in html
+    alert("Se debe llenar el campo Nombre.");
+    return false;
+  }
+  if (isImage(document.forms["categoryForm"]["image"].value)) { } else {
+    alert("La imagen debe ser formato jpg, jpeg, png, svg");
+    return false;
+  }
+
+  const categories = JSON.parse(localStorage.getItem('categories'));
+  const name = $('#name').val();
+  if (categories) {
+    for (var i = 0; i < categories.length; i++) {
+      if (categories[i].name == name) {
+
+        alert("La categoria ingresada ya está en uso.");
+        return false;
+      }
+    }
+  }
+
+  return true;
+
+}
+//check if category if being edited
+function loadDataOfEditCategory() {
+
+  let currentIDCategory = JSON.parse(sessionStorage.getItem('category'));
+
+  if (currentIDCategory) {//check if a product was selected
+    document.getElementById('title-name').innerHTML = "Editar Categoría";//change value of h1
+    let categories = JSON.parse(localStorage.getItem('categories'));//load all products
+    for (var i = 0; i < categories.length; i++) {
+      if (categories[i].id == currentIDCategory) {//compare id of product on list with id product of session storage
+        $('#name').val(categories[i].name);
+        $('#image').val(categories[i].image);
+
+
+      }
+    }
+
+
+
+
+  }
+
+
+}
+
+//load categories on home page
+function loadRecentCategories() {
+  let categories = JSON.parse(localStorage.getItem('categories'));
+  let categoriesContainer = "";
+  let categoriesContainerAdditional = "";
+  let counter = 2;
+
+  for (var i = categories.length - 1; i >= 0; i--) {
+    if (counter != 0) {
+      counter--;
+      categoriesContainer += `<div class="col-6 col-sm-6 col-xs-6">
+      <a  href="http://cambalachefinal.net:8080/dashboard-categorias.html" >
+    <img src="${categories[i].image}" class=" w-50" alt="img">
+    <p>${categories[i].name}</p></a></div>`;
+    } else {
+      categoriesContainerAdditional += `<div class="col-6 col-sm-6 col-xs-6 ">
+      <a  href="http://cambalachefinal.net:8080/dashboard-categorias.html">
+    <img src="${categories[i].image}" class=" w-50" alt="img">
+    <p>${categories[i].name}</p></a></div>`;
+      //onclick="saveCurrentProduct(${products[i].id});
+
+    }
+  }
+  document.getElementById('categoriesContainer').innerHTML = categoriesContainer;
+  document.getElementById('categoriesContainerAdditional').innerHTML = categoriesContainerAdditional;
+
+}
+
+
+function loadCategoriesSelect() {
+  // read categories from the database
+  const categories = JSON.parse(localStorage.getItem('categories'));
+  if (categories) {
+    let options =  `<option value="" disabled selected hidden>Categorías</option>`;
+    for (var i = 0; i < categories.length; i++) {
+      options += `<option value="${categories[i].id}">${categories[i].name}</option>`;
+
+    }
+    document.getElementById('category').innerHTML = options;
+  }
+}
+
 
 
